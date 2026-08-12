2a9798820b7b943b5ac17f1044b999fac08fd822
Jonathan Gruber <gruberjonathan@gmail.com>
2026-08-12 15:19:22 +0200

    Upgrade Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   outputDir: '.playwright',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  workers: 2,
   reporter: [
     ['list'],
     [
